1347e2f3dd384437398a30fba70dd019b28832c1
MAYTINH-NOLFT7K\MyPC <topinfo93@gmail.com>
2017-03-06 16:51:06 +0700

    fix js

--- resources/views/frontend/specials/richmond-hill-hot-properties.blade.php
+++ resources/views/frontend/specials/richmond-hill-hot-properties.blade.php
@@ -1,25 +1,6 @@
 @extends('layouts.frontend')
 
 @section('content')
-<style type="text/css">
-	#hot-in-market-properties .hot_properties_item_top .visite_libre, #recommended-properties .hot_properties_item_top .visite_libre{
-		border-top: 60px solid #ff0000;
-    	border-left: 60px solid #ff0000;
-	}
-	#hot-in-market-properties .hot_properties_item_bot p a, #recommended-properties .hot_properties_item_bot p a{
-		color: #ff0000;
-	}
-	.main_top_agents {
-	    background-size: cover;
-	    background-repeat: no-repeat;
-	    background: url(/images/top_agents_bg.jpg);
-	    text-align: center;
-	    height: 331px;
-	    padding: 15px 0 0 0;
-	}
-</style>
-
-
 <div class="row hot_properties featured_properties">
 <h2 class="title_hasline">FEATURED PROPERTIES</h2>
 	<div class="container">
@@ -295,7 +276,7 @@
 			<div class="col-sm-12">
 				<div class="hot_properties_overflow">
 					<div class="content_one_ads">
-						<div id="recommended-properties" class="owl-carousel owl-theme owl-loaded">
+						<div id="hot-in-market-properties" class="owl-carousel owl-theme owl-loaded">
 							<div class="item">
                                             <div class="hot_properties_item"><a href="http://realestate.homula.com/properties/q1-2016/" target="_blank">
                                               <div class="hot_properties_item_top">
@@ -1013,165 +994,5 @@
 
 @section('script')
 <script src="http://realestate.homula.com/wp-content/themes/realsite/assets/newhompage/js/owl.carousel.min.js" type="text/javascript"></script>
-<script>
-    	jQuery(document).ready(function(e) {
 
-
-
- 			jQuery(".owl-carousel").owlCarousel({
-				items : 6,
-				navigation : false,
-				slideBy: 1,
-				loop:true,
-				transitionStyle : "fade",
-				rewindSpeed: 100,
-				autoplay:true,
-				autoplayTimeout:2000,
-				autoplayHoverPause:true
-			});
-
-
-             jQuery("#featured-properties").owlCarousel({
-				items : 6,
-				navigation : false,
-				slideBy: 1,
-				loop:true,
-				transitionStyle : "fade",
-				rewindSpeed: 100,
-				autoplay:true,
-				autoplayTimeout:2000,
-				autoplayHoverPause:true
-			});
-			jQuery("#hot-in-market-properties").owlCarousel({
-				items : 6,
-				navigation : false,
-				slideBy: 1,
-				loop:true,
-				transitionStyle : "fade",
-				rewindSpeed: 100,
-				autoplay:true,
-	   		 	autoplayTimeout:2000,
-	   		 	autoplayHoverPause:true
-			});
-			jQuery("#last-month-properties").owlCarousel({
-				items : 6,
-				navigation : false,
-				slideBy: 1,
-				loop:true,
-				transitionStyle : "fade",
-				rewindSpeed: 100,
-				autoplay:true,
-	    		autoplayTimeout:2000,
-	    		autoplayHoverPause:true
-			});
-			jQuery("#recommended-properties").owlCarousel({
-				items : 6,
-				navigation : false,
-				slideBy: 1,
-				loop:true,
-				transitionStyle : "fade",
-				rewindSpeed: 100,
-				autoplay:true,
-	 		    autoplayTimeout:2000,
-	    		autoplayHoverPause:true
-			});
-			
-			// jQuery("#owl-demo-agent").owlCarousel({
-			// 	items : 5,
-			// 	navigation : false,
-			// 	slideBy: 1,
-			// 	loop:true,
-			// 	transitionStyle : "fade",
-			// 	rewindSpeed: 100,
-			// 	autoplay:true,
-	  //  			 autoplayTimeout:2000,
-	  //  			 autoplayHoverPause:true
-			// });
-			
-			// jQuery("#owl-demo-news").owlCarousel({
-			// 	items : 5,
-			// 	navigation : false,
-			// 	slideBy: 1,
-			// 	loop:true,
-			// 	transitionStyle : "fade",
-			// 	rewindSpeed: 100,
-			// 	dots:true,
-			// 	autoplay:true,
-	  //   		autoplayTimeout:2000,
-	  //   		autoplayHoverPause:true
-			// });
-			
-			/*jQuery('#featured-properties .owl-wrapper-outer').append('<div class="see-more"><p>SEE MORE<i class="fa fa-long-arrow-right" aria-hidden="true"></i></p></div>');
-			jQuery('#hot-in-market-properties .owl-wrapper-outer').append('<div class="see-more"><p>SEE MORE<i class="fa fa-long-arrow-right" aria-hidden="true"></i></p></div>');
-			jQuery('#last-month-properties .owl-wrapper-outer').append('<div class="see-more"><p>SEE MORE<i class="fa fa-long-arrow-right" aria-hidden="true"></i></p></div>');
-			jQuery('#recommended-properties .owl-wrapper-outer').append('<div class="see-more"><p>SEE MORE<i class="fa fa-long-arrow-right" aria-hidden="true"></i></p></div>');*/
-			jQuery('#featured-properties .owl-stage-outer').append('<div class="see-more"><p>SEE MORE<i class="fa fa-long-arrow-right" aria-hidden="true"></i></p></div>');
-			jQuery('#hot-in-market-properties .owl-stage-outer').append('<div class="see-more"><p>SEE MORE<i class="fa fa-long-arrow-right" aria-hidden="true"></i></p></div>');
-			jQuery('#last-month-properties .owl-stage-outer').append('<div class="see-more"><p>SEE MORE<i class="fa fa-long-arrow-right" aria-hidden="true"></i></p></div>');
-			jQuery('#recommended-properties .owl-stage-outer').append('<div class="see-more"><p>SEE MORE<i class="fa fa-long-arrow-right" aria-hidden="true"></i></p></div>');
-			jQuery('#owl-demo-agent .owl-stage-outer').append('<div class="see-more"><p>SEE MORE<i class="fa fa-long-arrow-right" aria-hidden="true"></i></p></div>');
-			jQuery('#owl-demo-news .owl-stage-outer').append('<div class="see-more"><p>SEE MORE<i class="fa fa-long-arrow-right" aria-hidden="true"></i></p></div>');
-			
-			
-			//var owl = $("#featured-properties");
-			 jQuery(document).on("click","#featured-properties .see-more", function(){
-				//jQuery("#featured-properties").trigger('owl.next');
-				//jQuery("#featured-properties .owl-next").trigger('click');
-				 var $dots = jQuery('#featured-properties .owl-dot');
-				 var $next = $dots.filter('.active').next();
-					if (!$next.length)
-						$next = $dots.first();
-					$next.trigger('click');
-					
-        	 });
-			 jQuery(document).on("click","#hot-in-market-properties .see-more", function(){
-            	//jQuery("#hot-in-market-properties").trigger('owl.next');
-				//jQuery("#hot-in-market-properties .owl-next").trigger('click');
-				 var $dots = jQuery('#hot-in-market-properties .owl-dot');
-				 var $next = $dots.filter('.active').next();
-					if (!$next.length)
-						$next = $dots.first();
-					$next.trigger('click');
-        	 });
-			 jQuery(document).on("click","#last-month-properties .see-more", function(){
-            	//jQuery("#last-month-properties").trigger('owl.next');
-				//jQuery("#last-month-properties .owl-next").trigger('click');
-				 var $dots = jQuery('#last-month-properties .owl-dot');
-				 var $next = $dots.filter('.active').next();
-					if (!$next.length)
-						$next = $dots.first();
-					$next.trigger('click');
-        	 });
-			 jQuery(document).on("click","#recommended-properties .see-more", function(){
-            	//jQuery("#recommended-properties").trigger('owl.next');
-				//jQuery("#recommended-properties .owl-next").trigger('click');
-				 var $dots = jQuery('#recommended-properties .owl-dot');
-				 var $next = $dots.filter('.active').next();
-					if (!$next.length)
-						$next = $dots.first();
-					$next.trigger('click');
-        	 });
-			 jQuery(document).on("click","#owl-demo-agent .see-more", function(){
-            	//jQuery("#owl-demo-agent").trigger('owl.next');
-				//jQuery("#owl-demo-agent .owl-next").trigger('click');
-				 var $dots = jQuery('#owl-demo-agent .owl-dot');
-				 var $next = $dots.filter('.active').next();
-					if (!$next.length)
-						$next = $dots.first();
-					$next.trigger('click');
-        	 });
-			 jQuery(document).on("click","#owl-demo-news .see-more", function(){
-            	//jQuery("#owl-demo-news").trigger('owl.next');
-				//jQuery("#owl-demo-news .owl-next").trigger('click');
-				 var $dots = jQuery('#owl-demo-news .owl-dot');
-				 var $next = $dots.filter('.active').next();
-					if (!$next.length)
-						$next = $dots.first();
-					$next.trigger('click');
-        	 });
-			 
-			 
-        });
-      
-    </script>
 @endsection
