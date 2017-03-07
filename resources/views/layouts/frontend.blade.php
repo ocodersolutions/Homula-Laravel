082af59b7bae6aa71c5abc1fb7d25a42ec4b50d9
Phan Văn Huy Hoàng <pvhuyhoang166@gmail.com>
2017-03-07 15:21:33 +0700

    update popup login

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -70,7 +70,7 @@
 				<!-- modal sign in -->
 				
 				<div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" id="modal_signin">
-					<div class="modal-dialog modal-lg" role="document">
+					<div class="modal-dialog modal-sm" role="document">
 						<div class="modal-content">
 							<div class="modal-header">
 								<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
